6b02f280a1fb12b3a02adaf8b9840a330acaff1b
theGiocatore <djordjemarcetic@gmail.com>
2020-01-03 20:23:28 +0100

    home done, about us in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
